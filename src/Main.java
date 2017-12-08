098bf5293708c0c2647803dde8f51c5038280286
danielchurch <daniellchurch@fvcc.edu>
2017-12-08 14:45:55 -0700

    Fix K means, provide example data to help verify it works

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Logger.level = Logger.LoggingLevel.IMPORTANT;
-        KMeans.kmeans(DataLoader.loadData("a1_raw.csv"), 8);
+        List<Centroid> centroids = KMeans.kmeans(genSamples(), 2);
+        // List<Centroid> centroids = KMeans.kmeans(DataLoader.loadData("a1_raw.csv"), 8);
+        centroids.stream().forEach(i -> {
+            System.out.println(i);
+        });
+    }
+
+    static List<List<Double>>  genSamples() {
+        List<List<Double>> values = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                List n = new ArrayList<>();
+                List n2 = new ArrayList<>();
+                n.add(i + 0.5);
+                n.add(j + 0.5);
+                n2.add(i + 50.5);
+                n2.add(j + 50.5);
+                values.add(n);
+                values.add(n2);
+            }
+        }
+
+        return values;
     }
 }
